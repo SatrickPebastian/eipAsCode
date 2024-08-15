6d9d0cd5226b97ee1d52cb6ba314c1897178f2cf
Stopper <patrick.stopper@capgemini.com>
2024-08-15 14:42:14 +0200

    Case Study examination

--- EICODA/rabbitMqModel.tf
+++ EICODA/rabbitMqModel.tf
@@ -24,11 +24,32 @@ resource "rabbitmq_queue" "OrderQueue" {
 }
 
 
-resource "rabbitmq_queue" "TaxQueue" {
-  name      = "TaxQueue"
+resource "rabbitmq_queue" "InvoiceQueue" {
+  name      = "InvoiceQueue"
   vhost     = "/"
   settings {
     durable    = true
+    auto_delete = false
+  }
+}
+
+
+resource "rabbitmq_queue" "ResultsQueue" {
+  name      = "ResultsQueue"
+  vhost     = "/"
+  settings {
+    durable    = true
+    auto_delete = false
+  }
+}
+
+
+resource "rabbitmq_exchange" "TranslatedTopic" {
+  name  = "TranslatedTopic"
+  vhost = "/"
+  settings {
+    type        = "topic"
+    durable     = true
     auto_delete = false
   }
 }
